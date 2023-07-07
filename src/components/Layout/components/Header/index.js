c4621fdb9f500d1d63a2f4cd0ff0fd6fff8dc6d1
Nguyen Dac Nguyen Binh <nguyendacnguyenbinh@gmail.com>
2023-07-07 10:37:03 +0700

    Dựng khung layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cs = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cs('wrapper')}>
+            <div className={cs('inner')}></div>
+        </header>
+    );
 }
 
 export default Header;
