dec81ce64f0492e1ab3b4ee8f7f98d9335f29735
nguyendacnguyenbinh <nguyendacnguyenbinh@gmail.com>
2023-07-28 12:29:02 +0700

    optimize header

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -54,7 +54,7 @@ const MENU_ITEMS = [
     },
 ];
 function Header() {
-    const currentUser = true;
+    const currentUser = false;
 
     //Handle logic
     const handleMenuChange = (menuItem) => {
